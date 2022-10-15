9fff2ab430efa43765e05659d65267171bf38b6a
Christimage <davmusty777@gmail.com>
2022-10-15 19:49:40 +0100

    made upload cleanups

--- src/common/services/cloudinary.ts
+++ src/common/services/cloudinary.ts
@@ -1,14 +1,16 @@
 import cloudinary from 'cloudinary';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
-console.log(CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET);
 
 const cloudinaryV2 = cloudinary.v2;
 
 cloudinaryV2.config({
-  cloud_name: 'book-with-me-react',
-  api_key: '159891148827824',
-  api_secret: 'SeRfIEz77gihdhInpcz1UNRdN94',
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_KEY,
+  api_secret: CLOUDINARY_SECRET,
 });
 
 export const cloudUpload = (file) => cloudinaryV2.uploader.upload(file);
